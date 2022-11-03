0c0505a19777ae409cfe9198cade0c89e296ae4d
franky <frankychantakyu@yahoo.com.hk>
2022-11-04 00:42:15 +1300

    init

---
+++ TDD/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace TDD
+{
+    public class Class1
+    {
+        [Fact]
+        public void Dummy_Test()
+        {
+            Assert.Equal(2, 1 + 1);
+        }
+    }
+}
