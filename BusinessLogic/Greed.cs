8867797c8c51c1e9eb86fe21f69e906aa7043128
franky <frankychantakyu@yahoo.com.hk>
2022-11-07 21:06:31 +1300

    adapt to new requirements for greed

--- BusinessLogic/Greed.cs
+++ BusinessLogic/Greed.cs
@@ -7,27 +7,50 @@
             if (dieValues.Count() == 0 || dieValues.Count() > 6) throw new InvalidDiceQuantity();
 
             var result = 0;
-            result += ScoreTriplet(dieValues);
-            result += ScoreIndividual(dieValues);
+
+            result += ScoreStraight(dieValues);
+            // has straight
+            if (result > 0) return result;
+
+            result += ScoreThreePairs(dieValues);
+            // has 3 pairs
+            if (result > 0) return result;
+
+            result += ScoreComboDice(dieValues);
+            result += ScoreNonComboDice(dieValues);
 
             return result;
         }
 
-        private int ScoreIndividual(params int[] dieValues)
+        private int ScoreNonComboDice(params int[] dieValues)
         {
             var result = 0;
             for(int dieValue = 1; dieValue < 7; dieValue++)
             {
                 var numOfMatchingDice = dieValues.Where(v => v == dieValue).Count();
-                var hasTriplet = numOfMatchingDice >= 3;
-                var numOfIndividuals = hasTriplet ? numOfMatchingDice - 3 : numOfMatchingDice;
+                var hasCombo = numOfMatchingDice >= 3;
+
+                // FIXME: 3 pairs combo is not yet recognized as combo in here
+                if (hasCombo || numOfMatchingDice is 0) continue;
 
                 if (dieValue == 1)
-                 result += 100 * numOfIndividuals;
+                 result += 100 * numOfMatchingDice;
 
                 if (dieValue == 5)
-                 result +=  50 * numOfIndividuals;
+                 result +=  50 * numOfMatchingDice;
             }
+
+            return result;
+        }
+
+        private int ScoreComboDice(params int[] dieValues)
+        {
+            var result = 0;
+            result += ScoreTriplet(dieValues);
+            result += ScoreFourOfAKind(dieValues);
+            result += ScoreFiveOfAKind(dieValues);
+            result += ScoreSixOfAKind(dieValues);
+            //result += ScoreThreePairs(dieValues);
 
             return result;
         }
@@ -38,7 +61,7 @@
             for(int dieValue = 1; dieValue < 7; dieValue++)
             {
                var numOfMatchingDice = dieValues.Where(v => v == dieValue).Count();
-               var hasTriplet = numOfMatchingDice >= 3;
+               var hasTriplet = numOfMatchingDice == 3;
 
                if (!hasTriplet) continue;
 
@@ -49,6 +72,91 @@
             }
 
             return result;
+        }
+
+        private int ScoreFourOfAKind(params int[] dieValues)
+        {
+            var result = 0;
+            for(int dieValue = 1; dieValue < 7; dieValue++)
+            {
+               var numOfMatchingDice = dieValues.Where(v => v == dieValue).Count();
+               var hasFourOfAKind = numOfMatchingDice == 4;
+
+               if (!hasFourOfAKind) continue;
+
+               if (dieValue == 1)
+                  result += 1000 * 2;
+               else
+                 result += dieValue * 100 * 2;
+            }
+
+            return result;
+        }
+
+        private int ScoreFiveOfAKind(params int[] dieValues)
+        {
+            var result = 0;
+            for (int dieValue = 1; dieValue < 7; dieValue++)
+            {
+                var numOfMatchingDice = dieValues.Where(v => v == dieValue).Count();
+                var hasFourOfAKind = numOfMatchingDice == 5;
+
+                if (!hasFourOfAKind) continue;
+
+                if (dieValue == 1)
+                    result += 1000 * 4;
+                else
+                    result += dieValue * 100 * 4;
+            }
+
+            return result;
+        }
+
+        private int ScoreSixOfAKind(params int[] dieValues)
+        {
+            var result = 0;
+            for (int dieValue = 1; dieValue < 7; dieValue++)
+            {
+                var numOfMatchingDice = dieValues.Where(v => v == dieValue).Count();
+                var hasFourOfAKind = numOfMatchingDice == 6;
+
+                if (!hasFourOfAKind) continue;
+
+                if (dieValue == 1)
+                    result += 1000 * 8;
+                else
+                    result += dieValue * 100 * 8;
+            }
+
+            return result;
+        }
+
+        private int ScoreThreePairs(params int[] dieValues)
+        {
+            var result = 0;
+            var pairs = 0;
+            for (int dieValue = 1; dieValue < 7; dieValue++)
+            {
+                var numOfMatchingDice = dieValues.Where(v => v == dieValue).Count();
+                var hasPair = numOfMatchingDice == 2;
+
+                if(hasPair)
+                    pairs++;
+            }
+
+            if (pairs == 3)
+                result = 800;
+
+            return result;
+        }
+
+        private int ScoreStraight(params int[] dieValues)
+        {
+            var straightSequence = new int[] { 1, 2, 3, 4, 5, 6 };
+
+            if (straightSequence.SequenceEqual(dieValues)) return 1200;
+
+            return 0;
         }
     }
 
--- BusinessLogic/GreedTest.cs
+++ BusinessLogic/GreedTest.cs
@@ -31,9 +31,9 @@ namespace BusinessLogic
         }
 
         [Theory]
-        [InlineData(1150, 1,1,1,5,1)]
-        [InlineData(300, 2,3,3,3,2)]
-        [InlineData(500, 3,5,5,5,3)]
+        [InlineData(1050, 1, 3, 1, 5, 1)]
+        [InlineData(300, 2, 3, 3, 3, 2)]
+        [InlineData(500, 3, 5, 5, 5, 3)]
         public void Given_Five_Dice_Values_With_Triplet_Combo_When_Call_Score_Then_Return_Correct_Scores(int expectedScores, params int[] dieValues)
         {
             var greed = new Greed();
@@ -44,9 +44,9 @@ namespace BusinessLogic
         }
 
         [Theory]
-        [InlineData(150, 1,2,3,4,5)]
-        [InlineData(250, 1,1,3,4,5)]
-        [InlineData(300, 1,1,3,5,5)]
+        [InlineData(150, 1, 2, 3, 4, 5)]
+        [InlineData(250, 1, 1, 3, 4, 5)]
+        [InlineData(300, 1, 1, 3, 5, 5)]
         public void Given_Five_Dice_Values_Without_Triplet_Combo_When_Call_Score_Then_Return_Correct_Scores(int expectedScores, params int[] dieValues)
         {
             var greed = new Greed();
@@ -54,6 +54,67 @@ namespace BusinessLogic
             var actualScores = greed.Score(dieValues);
 
             actualScores.Should().Be(expectedScores);
+        }
+
+        [Theory]
+        [InlineData(1000 * 2, 1,1,1,3,1,3)]
+        [InlineData(200 * 2, 2,2,2,3,2,3)]
+        [InlineData(600 * 2, 6,6,6,3,6,3)]
+        public void Given_Six_Dice_Values_With_Four_Of_A_Kind_When_Call_Score_Then_Return_Correct_Scores(int expectedScores, params int[] dieValues)
+        {
+            var greed = new Greed();
+
+            var actualScores = greed.Score(dieValues);
+
+            actualScores.Should().Be(expectedScores);
+        }
+
+        [Theory]
+        [InlineData(1000 * 4, 1,1,1,1,1,3)]
+        [InlineData(200 * 4, 2,2,2,2,2,3)]
+        [InlineData(600 * 4, 6,6,6,6,6,3)]
+        public void Given_Six_Dice_Values_With_Five_Of_A_Kind_When_Call_Score_Then_Return_Correct_Scores(int expectedScores, params int[] dieValues)
+        {
+            var greed = new Greed();
+
+            var actualScores = greed.Score(dieValues);
+
+            actualScores.Should().Be(expectedScores);
+        }
+
+        [Theory]
+        [InlineData(1000 * 8, 1,1,1,1,1,1)]
+        [InlineData(200 * 8, 2,2,2,2,2,2)]
+        [InlineData(600 * 8, 6,6,6,6,6,6)]
+        public void Given_Six_Dice_Values_With_Six_Of_A_Kind_When_Call_Score_Then_Return_Correct_Scores(int expectedScores, params int[] dieValues)
+        {
+            var greed = new Greed();
+
+            var actualScores = greed.Score(dieValues);
+
+            actualScores.Should().Be(expectedScores);
+        }
+
+        [Theory]
+        [InlineData(800, 2,2,3,3,4,4)]
+        [InlineData(800, 1,1,3,3,6,6)]
+        public void Given_Six_Dice_Values_With_Three_Pairs_When_Call_Score_Then_Return_Correct_Scores(int expectedScores, params int[] dieValues)
+        {
+            var greed = new Greed();
+
+            var actualScores = greed.Score(dieValues);
+
+            actualScores.Should().Be(expectedScores);
+        }
+
+        [Fact]
+        public void Given_Six_Dice_Values_With_Straight_When_Call_Score_Then_Return_Correct_Scores()
+        {
+            var greed = new Greed();
+
+            var actualScores = greed.Score(1,2,3,4,5,6);
+
+            actualScores.Should().Be(1200);
         }
     }
 }
