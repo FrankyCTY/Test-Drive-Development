ab1205ecfdaad34f8d168d505850d63500119238
franky <frankychantakyu@yahoo.com.hk>
2022-11-07 16:28:48 +1300

    finish simple greed game with TDD

--- BusinessLogic/Greed.cs
+++ BusinessLogic/Greed.cs
@@ -1,11 +1,54 @@
-﻿using System;
-namespace BusinessLogic
+﻿namespace BusinessLogic
 {
     public class Greed
     {
-        public void RollDice()
-        { }
+        public int RollDices(params int[] dieValues)
+        {
+            //if (dieValues.Count() < 5) throw new InvalidDiceQuantity();
 
+            var scores = 0;
+
+            for(int dieValue = 1; dieValue < 7; dieValue++)
+            {
+                var numOfMatchingDice = Array.FindAll(dieValues, (v => v == dieValue)).Count();
+                if (numOfMatchingDice is 0) continue;
+
+                var hasTriplet = numOfMatchingDice >= 3;
+                var numOfIndividuals = numOfMatchingDice;
+
+                if (hasTriplet)
+                {
+                    scores += CalculateTripletScores(dieValue);
+                    numOfIndividuals = numOfMatchingDice - 3;
+                }
+
+                scores += CalculateIndividualScores(dieValue, numOfIndividuals);
+            }
+
+            return scores;
+        }
+
+        private int CalculateIndividualScores(int dieValue, int numOfIndividuals)
+        {
+            if (dieValue == 1)
+                return 100 * numOfIndividuals;
+
+            if (dieValue == 5)
+                return  50 * numOfIndividuals;
+
+            return 0;
+        }
+
+        private int CalculateTripletScores(int dieValue)
+        {
+            if (dieValue == 1)
+                return 1000;
+
+            return dieValue * 100;
+        }
     }
+
+    public class InvalidDiceQuantity : Exception
+    { }
 }
 
--- BusinessLogic/GreedTest.cs
+++ BusinessLogic/GreedTest.cs
@@ -1,16 +1,73 @@
-﻿using System;
+﻿using FluentAssertions;
 using Xunit;
 
 namespace BusinessLogic
 {
     public class GreedTest
     {
-        [Fact]
-        public void Can_Call_Add()
+        //[Fact]
+        //public void Given_RollDices_Is_Called_When_Less_Then_5_DiceValues_Passed_In_Then_Throw_InvalidDiceQuantity()
+        //{
+        //    var greed = new Greed();
+
+        //    Action RollDices = () => greed.RollDices(1);
+
+        //    RollDices.Should().ThrowExactly<InvalidDiceQuantity>();
+        //}
+
+        [Theory]
+        [InlineData(100, 1)]
+        [InlineData(50, 5)]
+        [InlineData(0, 6)]
+        public void Given_Single_Die_When_Rolled_Then_Return_Correct_Scores(int expectedPoints, int dieValue)
         {
             var greed = new Greed();
 
-            greed.RollDice();
-	    }
+            var actualScores = greed.RollDices(dieValue);
+
+            actualScores.Should().Be(expectedPoints);
+        }
+
+        [Theory]
+        [InlineData(1000, 1,1,1)]
+        [InlineData(200, 2,2,2)]
+        [InlineData(300, 3,3,3)]
+        [InlineData(500, 5,5,5)]
+        [InlineData(600, 6,6,6)]
+        public void Given_3_dices_When_Rolled_Triplet_Values_Then_Return_Correct_Scores(int expectedPoints, params int[] dieValues)
+        {
+            var greed = new Greed();
+
+            var actualScores = greed.RollDices(dieValues);
+
+            actualScores.Should().Be(expectedPoints);
+        }
+
+        [Theory]
+        [InlineData(200, 2,2,2,2,2)]
+        [InlineData(1200, 1,1,1,1,1)]
+        [InlineData(600, 5,5,5,5,5)]
+        [InlineData(600, 6,6,6,6,6)]
+        public void Given_5_Dice_With_Same_Value_When_Rolled_Then_Return_Correct_Scores(int expectedPoints, params int[] dieValues)
+        {
+            var greed = new Greed();
+
+            var actualScores = greed.RollDices(dieValues);
+
+            actualScores.Should().Be(expectedPoints);
+        }
+
+        [Theory]
+        [InlineData(1150, 1,1,1,5,1)]
+        [InlineData(0, 2,3,4,6,2)]
+        [InlineData(350, 3,4,5,3,3)]
+        public void Given_5_dices_When_Rolled_Then_Return_Correct_Scores(int expectedScores, params int[] dieValues)
+        {
+            var greed = new Greed();
+
+            var actualScores = greed.RollDices(dieValues);
+
+            actualScores.Should().Be(expectedScores);
+        }
     }
 }
